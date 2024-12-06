2aca04d32fe1dafa12ea26653ac7a494578f93f5
Mohamad Rizki Dafianto <mohamad.rizki.dafianto@gmail.com>
2024-12-06 10:25:34 +0700

    feat: add shared dependencies

---
+++ shared/src/commonMain/kotlin/org/jetbrains/greeting/NewYear.kt
@@ -0,0 +1,17 @@
+package org.jetbrains.greeting
+
+import kotlinx.datetime.Clock
+import kotlinx.datetime.LocalDate
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.daysUntil
+import kotlinx.datetime.todayIn
+
+fun daysUntilNewYear(): Int {
+    val today = Clock.System.todayIn(TimeZone.currentSystemDefault())
+    val closestNewYear = LocalDate(today.year + 1, 1, 1)
+    return today.daysUntil(closestNewYear)
+}
+
+fun remainingDaysPhrases(): String {
+    return "There are only ${daysUntilNewYear()} days left until New Year!"
+}
